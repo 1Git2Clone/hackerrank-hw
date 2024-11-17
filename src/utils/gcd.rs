041e87b11ac4eaaf5fb45ecd14fcd7729edb4241
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-11-17 15:04:29 +0200

    Changed ShrAssign to i8 from usize

--- src/utils/gcd.rs
+++ src/utils/gcd.rs
@@ -7,7 +7,7 @@ use num_traits::PrimInt;
 
 pub trait Gcd
 where
-    Self: PrimInt + ShrAssign<usize> + SubAssign,
+    Self: PrimInt + ShrAssign<i8> + SubAssign,
 {
     /// NOTE: Exists for the internal LCM implementation.
     #[deprecated(note = "use crate::utils::gcd::Gcd::gcd() instead.")]
@@ -24,7 +24,7 @@ where
                 swap(&mut self, &mut other)
             }
             other -= self;
-            other >>= other.trailing_zeros() as usize;
+            other >>= other.trailing_zeros() as i8;
         }
 
         self << k
